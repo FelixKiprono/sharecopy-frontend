402fc6b18937d04cf6dbf45a7af92f0d9778638f
felix <felixkiprono@yahoo.com>
2020-02-14 01:34:22 -0800

    hidden users

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -45,11 +45,12 @@
            
               
             
-            <li class="nav-item">
+         <!--    <li class="nav-item">
              <router-link
              class="nav-link"
               to="/user">User</router-link>
-              </li> 
+              </li>  -->
+
             <li class="nav-item">
               <b-link  class="nav-link" v-b-modal.modal-1>Contact us</b-link>
               <b-modal id="modal-1" title="Contact">
